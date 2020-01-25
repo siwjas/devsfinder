046de5d7b2cbc68940d41b504a76191680e19dfc
Wellington Almeida <wjas@globo.com>
2020-01-25 00:52:34 -0300

    form and auto fill position (latitude, longitude)

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
 import './global.css'
+import './Sidebar.css'
+import './Main.css'
 import './App.css'
 
 function App() {
+  const [latitude, setLatitude]   = useState('')
+  const [longitude, setLongitude] = useState('')
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords
+        setLatitude(latitude)
+        setLongitude(longitude)
+      },
+      (err) => {
+        console.log(err)
+      },
+      {
+        timeout: 120000,
+      } 
+    )
+  }, [])
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastre-se</strong>
         <form>
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
             <input name="github_username" id="github_username" required />
           </div>
@@ -21,13 +42,25 @@ function App() {
           
           <div className="input-group">
             <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <label type="number" htmlFor="latitude">Latitude</label>
+              <input 
+              name="latitude" 
+              id="latitude" 
+              required 
+              value={latitude} 
+              onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
-            <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+            <label type="number" htmlFor="longitude">Longitude</label>
+              <input 
+              name="longitude" 
+              id="longitude" 
+              required 
+              value={longitude} 
+              onChange={e => setLongitude(e.target.value)}
+              />
             </div>              
           </div>
 
@@ -36,7 +69,55 @@ function App() {
       </aside>
 
       <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/57413904?v=4" alt=""/>
+              <div className="user-info">
+                <strong>Wellington Almeida</strong>
+                <span>ReactJS, NodeJS, Ruby, Rails</span>
+              </div>
+            </header>
+            <p>Entusiasta de tecnologia e apaixonado por programação.</p>
+            <a href="https://github.com/siwjas" target="_blank" rel="noopener noreferrer">Perfil Github</a>
+          </li>
 
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/57413904?v=4" alt=""/>
+              <div className="user-info">
+                <strong>Wellington Almeida</strong>
+                <span>ReactJS, NodeJS, Ruby, Rails</span>
+              </div>
+            </header> 
+            <p>Entusiasta de tecnologia e apaixonado por programação.</p>
+            <a href="https://github.com/siwjas" target="_blank" rel="noopener noreferrer">Perfil Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/57413904?v=4" alt=""/>
+              <div className="user-info">
+                <strong>Wellington Almeida</strong>
+                <span>ReactJS, NodeJS, Ruby, Rails</span>
+              </div>
+            </header>
+            <p>Entusiasta de tecnologia e apaixonado por programação.</p>
+            <a href="https://github.com/siwjas" target="_blank" rel="noopener noreferrer">Perfil Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/57413904?v=4" alt=""/>
+              <div className="user-info">
+                <strong>Wellington Almeida</strong>
+                <span>ReactJS, NodeJS, Ruby, Rails</span>
+              </div>
+            </header>
+            <p>Entusiasta de tecnologia e apaixonado por programação.</p>
+            <a href="https://github.com/siwjas" target="_blank" rel="noopener noreferrer">Perfil Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   )
